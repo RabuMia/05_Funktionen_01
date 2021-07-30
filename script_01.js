3162873427b3b948cb74c35c5b1fdfc079a6d078
RabuMia <Excelchen@hotmail.de>
2021-07-30 10:45:24 +0200

    Kapselung von Codeblöcken++

--- script_01.js
+++ script_01.js
@@ -1,2 +1,25 @@
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// // Funktionsrumpf (body) | callee
+// function test()
+// {
+//     console.log("Hallo Kevin!");
+// }
+
+// // Funktionsaufruf (call)
+// test();
+
+/**** Funktionen 02a ****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeNamen() 
+{
+    const firstName = "Kevin"; // Variable --> lokal scope (Geltungsbereich)
+    console.log("Hallo " + firstName + "!");
+  
+}
+
+ausgabeNamen();
+console.log(firstName); // Fehler --> lokal!!!!
